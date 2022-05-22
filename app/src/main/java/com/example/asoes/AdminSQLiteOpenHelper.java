0e89341e04586949870e121a54847b6df06f5337
Luis C <19041240@itdurango.edu.mx>
2022-05-22 00:51:37 -0500

    Asoes

--- app/src/main/java/com/example/asoes/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/asoes/AdminSQLiteOpenHelper.java
@@ -8,18 +8,18 @@ import androidx.annotation.Nullable;
 
 public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
 
-
     public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase BaseDeDatos) {
+        BaseDeDatos.execSQL("create table asesoria(Id_Asesoria int primary key,Docente text, Horario text, Link text,Asignatura text,Recurso text)");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase BaseDeDatos, int i, int i1) {
+        BaseDeDatos.execSQL("DROP TABLE IF EXISTS asesoria");
+        onCreate(BaseDeDatos);
     }
 }
