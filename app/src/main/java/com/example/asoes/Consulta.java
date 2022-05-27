d2516fb0edaab929ef1b24b576f31e7a0f56b313
Luis C <19041240@itdurango.edu.mx>
2022-05-26 20:56:19 -0500

    consulta de info

--- app/src/main/java/com/example/asoes/Consulta.java
+++ app/src/main/java/com/example/asoes/Consulta.java
@@ -2,12 +2,21 @@ package com.example.asoes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Consulta extends AppCompatActivity {
 
@@ -31,7 +40,7 @@ public class Consulta extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-
+consultarLista();
 
             }
         });
@@ -39,29 +48,24 @@ public class Consulta extends AppCompatActivity {
     }
 
 
+    public void consultarLista() {
+        try{
+            CONEXION conex = new CONEXION();
+            Statement stm = conex.conexion().createStatement();
+            ResultSet rs = stm.executeQuery("SELECT * FROM ASOESPERSONAS");
+
+            if(rs.next()){
+                tVDatos.setText("Asesoría: "+rs.getString(2)+"\n"+"Instructor: "+rs.getString(3)+"\n"+"Horario: "+rs.getString(4)+"\n"+"Enlace de sesión: "+rs.getString(5)+"\n"+"Recursos didácticos: "+rs.getString(6));
+                tVDatos.setText("------------------------------------------------------------");
+         //       tVDatos.setText(rs.getString(3));
+         //       tVDatos.setText(rs.getString(4));
+         //       tVDatos.setText(rs.getString(5));
+         //       tVDatos.setText(rs.getString(6));
+            }
 
 
-
-
-
-
-
-
-    private void consultarLista() {
-        admin = new AdminSQLiteOpenHelper(this, "Asoes", null, 2);
-        SQLiteDatabase BaseDeDatos = admin.getReadableDatabase();
-
-
-        Cursor cursor = BaseDeDatos.rawQuery
-                ("select Id_Asesoria, Docente, Horario, Link, Asignatura, Recurso from asesoria", null);
-
-        //   if(cursor.moveToFirst()){
-
-        tVDatos.setText(cursor.getInt(0) +"\n" + cursor.getString(1) +"\n" + cursor.getString(2) +"\n"+ cursor.getString(3) +"\n"+ cursor.getString(4) +"\n"+ cursor.getString(5));
-
-
-        BaseDeDatos.close();
-
-        //   }
+        }catch (Exception e){
+            Toast.makeText(this, "Error viejo", Toast.LENGTH_SHORT).show();
+        }
     }
 }
